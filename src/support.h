54f57b95366ef31ae8dc832e0170efb66de58a43
Paul Colomiets <paul@colomiets.name>
2012-07-19 09:19:49 +0000

    Few fixes to work with gcc 4.5

--- src/support.h
+++ src/support.h
@@ -1,17 +1,17 @@
 #ifndef _H_SUPPORT_
 #define _H_SUPPORT_
 
-typedef struct config_socket_s config_socket_t;
-typedef struct context context;
-typedef struct message message;
+struct config_socket_s;
+struct message;
+struct context;
 
 typedef struct socket_impl {
-    int (*close)(config_socket_t *sock);
-    int (*get_fd)(config_socket_t *sock);
-    int (*check_state)(config_socket_t *sock);
-    int (*write_string)(config_socket_t *sock, const char *string, int more);
-    int (*write_message)(config_socket_t *sock, message *msg);
-    int (*read_message)(config_socket_t *sock, message *msg);
+    int (*close)(struct config_socket_s *sock);
+    int (*get_fd)(struct config_socket_s *sock);
+    int (*check_state)(struct config_socket_s *sock);
+    int (*write_string)(struct config_socket_s *sock, const char *string, int more);
+    int (*write_message)(struct config_socket_s *sock, struct message *msg);
+    int (*read_message)(struct config_socket_s *sock, struct message *msg);
 } socket_impl;
 
 typedef struct socket_state {
